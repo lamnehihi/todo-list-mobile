e26a2881d22862a7f4cc4099c03b60effd82fd8f
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-05-22 22:43:50 +0700

    8-render-a-list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem';
+import { render } from '@testing-library/react';
 
-function App() {
-  const todos = ["Go to market", "Buy food", "Make dinner"];
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <TodoItem title="go to gym"/>
-          <TodoItem title="come back home"/>
-          <TodoItem title="studying"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = ["Go to market", "Buy food", "Make dinner"];
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            this.todoItems.map((item, index) => <TodoItem key={index} title={item}/>)  
+          }
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
