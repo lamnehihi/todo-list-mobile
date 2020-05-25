4c600fcf7bcae1d1c3c222d0015c16bb919c6e7f
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-05-25 21:26:35 +0700

    10-Dynamic-class-name

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TodoItem from './components/TodoItem';
-import { render } from '@testing-library/react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoItem from "./components/TodoItem";
+import { render } from "@testing-library/react";
 
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = ["Go to market", "Buy food", "Make dinner"];
+    this.todoItems = [
+      { title: "Go to market", isComplete: true },
+      { title: "Buy food" , isComplete: true},
+      { title: "Make dinner" },
+    ];
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {
-            this.todoItems.map((item, index) => <TodoItem key={index} title={item}/>)  
-          }
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.todoItems.map((item, index) => (
+          <TodoItem key={index} item={item} />
+        ))}
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import "./TodoItem.css";
 
 class TodoItem extends Component {
   render() {
+    const item = this.props.item
+    const classNames = require('classnames');
+    let className= "TodoItem";
+    if(item.isComplete) {
+      className = classNames(className, ' TodoItem-complete');
+    }
     return (
-      <div className="todoItems">
-        <p>{this.props.title}</p>
+      <div className={className}>
+        <p>{item.title}</p>
       </div>
     );
   }
