e03f3695704b10b28bd0267df4e9a03b76ad1830
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-05-31 12:53:29 +0700

    change isComplete when clicked

--- src/App.js
+++ src/App.js
@@ -7,20 +7,31 @@ import { render } from "@testing-library/react";
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
-      { title: "Go to market", isComplete: true },
-      { title: "Buy food" , isComplete: true},
-      { title: "Make dinner" },
-    ];
+    this.state = {
+      todoItems : [
+        { title: "Go to market", isComplete: true },
+        { title: "Buy food" , isComplete: true},
+        { title: "Make dinner" },
+      ]
+    }
+  }
+  itemClick() {
+    this.setState({
+      todoItems : [
+        this.props.item.isComplete = !this.props.item.isComplete
+      ]
+    });
   }
   render() {
+    const todoItems = this.state.todoItems;
     return (
       <div className="App">
-        {this.todoItems.length > 0 && this.todoItems.map((item, index) => (
-          <TodoItem key={index} item={item} />
+        {
+          todoItems.length > 0 && todoItems.map((item, index) => (
+          <TodoItem key={index} item={item} onClick={this.itemClick}/>
         ))}
         {
-          this.todoItems.length === 0 && <p>Nothing here!</p>
+          todoItems.length === 0 && <p>Nothing here!</p>
         }
       </div>
     );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,14 @@ import "./TodoItem.css";
 
 class TodoItem extends Component {
   render() {
-    const item = this.props.item
+    const item = this.props.item;
     const classNames = require('classnames');
     let className= "TodoItem";
     if(item.isComplete) {
       className = classNames(className, ' TodoItem-complete');
     }
     return (
-      <div className={className}>
+      <div onClick={this.props.onClick.bind(this)} className={className}>
         <p>{item.title}</p>
       </div>
     );
