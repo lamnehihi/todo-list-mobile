68c4356f71d5b826163295d1ce0b8b5b9f42161a
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-06-02 21:13:41 +0700

    input new todo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import TodoItem from "./components/TodoItem";
 import { render } from "@testing-library/react";
+import downArrow from "./down-arrow.svg";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      newItem: "",
       todoItems: [
         { title: "Go to market", isComplete: true },
         { title: "Buy food", isComplete: true },
         { title: "Make dinner" },
       ],
     };
+
+    this.addItem = this.addItem.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
   itemClick(item) {
     return (event) => {
@@ -32,15 +36,42 @@ class App extends Component {
       });
     };
   }
+
+  addItem(event) {
+    if (event.keyCode === 13) {
+      const value = event.target.value;
+      if (value === "") return;
+      if (value.trim() === "") return;
+      this.setState({
+        newItem : '',
+        todoItems: [{ title: value.trim() }, ...this.state.todoItems],
+      });
+      event.target.value = "";
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      newItem : event.target.value
+    });
+  }
+
   render() {
     const todoItems = this.state.todoItems;
     return (
-      <div >
+      <div>
         <h1>todos</h1>
-        <div className="input">
-
-        </div>
         <div className="App">
+          <div className="input">
+            <img src={downArrow}/>
+            <input
+              type="text"
+              placeholder="What needs to be done?"
+              value={this.state.newItem}
+              onChange={this.onChange}
+              onKeyUp={this.addItem}
+            ></input>
+          </div>
           {todoItems.length > 0 &&
             todoItems.map((item, index) => (
               <TodoItem
